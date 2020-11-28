c4cc47ebac4487d291047b6410d89e741ff4ef5d
eahenle <eahenle@gmail.com>
2020-11-27 19:10:45 -0800

    Update bonds.jl

--- test/bonds.jl
+++ test/bonds.jl
@@ -14,7 +14,7 @@ function visual_check(xtal::String)
     @info c.name * " see .vtk and .xyz to visually check bonds"
 end
 
-@testset "Bond Tests" begin
+@testset "NiPyC2 Tests" begin
     # NiPyC2 bonding
     c = Crystal("NiPyC2_relax.cif")
     strip_numbers_from_atom_labels!(c)
@@ -24,32 +24,49 @@ end
                      BondingRule(:Ni, :N, 0.4, 2.5),
                      BondingRule(:*, :*, 0.4, 1.9)]
     infer_bonds!(c, true, bonding_rules)
-    g1 = deepcopy(c.bonds)
+    c1 = deepcopy(c)
     conn_comps = connected_components(c.bonds)
+
     @test length(conn_comps) == 2 # interpenetrated
+
     c_red = getindex(c, conn_comps[1])
     c_blue = getindex(c, conn_comps[2])
+
     @test ne(c_red.bonds) + ne(c_blue.bonds) == ne(c.bonds)
+
     remove_bonds!(c)
     infer_geometry_based_bonds!(c, true)
-    @test length(conn_comps) == 2 # interpenetrated
-    # FAILING TEST probably need different bonding_rules to match Cordero results
-    @test g1 == c.bonds # consistency between two different bonding schemes
 
+    @test length(conn_comps) == 2 # interpenetrated
+
+    # debugging outputs (delete me)
+    write_bond_information.([c, c1], ["c", "c1"])
+    write_xyz(c, "c")
+
+    @test c1.bonds == c.bonds # consistency between two different bonding schemes
+
+end
+@testset "FIQCEN Tests" begin
     # FIQCEN bonding
     c = Crystal("FIQCEN_clean.cif")
     strip_numbers_from_atom_labels!(c)
     infer_geometry_based_bonds!(c, true)
-    # FAILING TEST probably need different bonding_rules to match Cordero results
+
     @test length(connected_components(c.bonds)) == 1 # not interpenetrated
+
     @test c.atoms.species[neighbors(c.bonds, 1)] == [:Cu, :O, :O, :O, :O]
+
     visual_check("FIQCEN_clean.cif")
+
     # reduce covalant radius to see Cu-Cu bond disappear
     cordero_params = cordero_parameters()
     cordero_params[:Cu] = Dict(:radius_Å => 1.15, :esd_pm => 4.)
     remove_bonds!(c)
+
     @test ne(c.bonds) == 0
+
     infer_geometry_based_bonds!(c, true, cordero_params=cordero_params)
+
     @test c.atoms.species[neighbors(c.bonds, 1)] == [:O, :O, :O, :O]
 end
 end
