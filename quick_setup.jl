8b7054404eee06dc06ff62b9bd5279fed6bc060b
Adrian Henle <eahenle@gmail.com>
2021-05-30 12:53:19 -0700

    quick setup

--- quick_setup.jl
+++ quick_setup.jl
@@ -1,18 +1,28 @@
 @info "Running Xtals.jl quick-setup."
 
 import Pkg
-Pkg.build()
 
-@info "Setting up Python dependencies..."
-ENV["PYTHON"]=""
-Pkg.add("PyCall")
-Pkg.build("PyCall")
-Pkg.add("Conda")
-using Conda
-Conda.add("scipy")
+@info "Checking Python dependencies..."
+try # see if scipy is already available
+    using PyCall
+    pyimport("scipy")
+catch # if not, set up Python environment and get scipy
+    @info "Setting up Python dependencies..."
+    ENV["PYTHON"]=""
+    Pkg.add("PyCall")
+    Pkg.build("PyCall")
+    Pkg.add("Conda")
+    using Conda
+    Conda.add("scipy")
+end
 
-@info "Setting up Xtals..."
-Pkg.add("Xtals")
-Pkg.build("Xtals")
+@info "Checking Xtals..."
+try # see if Xtals is already installed
+    using Xtals
+catch # if not, install it
+    @info "Installing Xtals..."
+    Pkg.add("Xtals")
+    Pkg.build("Xtals")
+end
 
 @info "Done!"
