f35855e1ce4a7025db9e3b9d19c1f4828c33a9e5
Adrian Henle <eahenle@gmail.com>
2021-09-29 15:45:46 -0700

    Docstring fix (#103)
    * fix infer_bonds! docstring
    
    * docstring fix

--- src/bonds.jl
+++ src/bonds.jl
@@ -192,21 +192,18 @@ end
 
 
 """
-    infer_bonds!(crystal, include_bonds_across_periodic_boundaries, bonding_rules=nothing)
+    infer_bonds!(crystal, include_bonds_across_periodic_boundaries; bonding_rules=rc[:bonding_rules])
 
-Populate the bonds in the crystal object based on the bonding rules. If a
-pair doesn't have a suitable rule then they will not be considered bonded.
+Populate the bonds in the crystal object based on the bonding rules. If a pair doesn't have a suitable rule then they will not be considered bonded.
 
-`:*` is considered a wildcard and can be substituted for any species. It is a
-good idea to include a bonding rule between two `:*` to allow any atoms to bond
-as long as they are close enough.
+`:*` is considered a wildcard and can be substituted for any species. It is a good idea to include a bonding rule between two `:*` to allow any atoms to bond as long as they are close enough.
 
 The bonding rules are hierarchical, i.e. the first bonding rule takes precedence over the latter ones.
 
 # Arguments
-- `crystal::Crystal`: The crystal that bonds will be added to
-- `include_bonds_across_periodic_boundaries::Bool`: Whether to check across the periodic boundary when calculating bonds
-- `bonding_rules::Union{Array{BondingRule, 1}, Nothing}=nothing`: The array of bonding rules that will be used to fill the bonding information. They are applied in the order that they appear. if `nothing`, default bonding rules will be applied; see [`get_bonding_rules`](@ref)
+- `crystal::Crystal`: The crystal that bonds will be added to.
+- `include_bonds_across_periodic_boundaries::Bool`: Whether to check across the periodic boundary when calculating bonds.
+- `bonding_rules::Array{BondingRule, 1}`: The array of bonding rules that will be used to fill the bonding information. They are applied in the order that they appear. `rc[:bonding_rules]` will be used if none provided.
 - `calculate_vectors::Bool`: Optional. Set `true` to annotate all edges in the `bonds` graph with vector information.
 """
 function infer_bonds!(crystal::Crystal, include_bonds_across_periodic_boundaries::Bool;
