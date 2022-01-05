9f9b887cf94fc07c195c26d18aedb5de7e80dc5e
eahenle <eahenle@gmail.com>
2022-01-05 13:20:55 -0800

    fix version check

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,8 @@
-version_pattern = "[0-9].[0-9].[0-9]"
-binary_path_string = match(Regex("julia-$version_pattern/bin/julia"), ENV["_"]).match
-version_string = match(Regex("$version_pattern"), binary_path_string).match
-major_version, minor_version, _ = parse.(Int, split(version_string, "."))
+@assert VERSION.major == 1
+@assert VERSION.minor ≥ 0
 
-@assert major_version == 1
-@assert minor_version ≥ 5
 include("unit_tests.jl")
 
-
-if minor_version == 7
+if VERSION.minor == 7
     include("doc_tests.jl")
 end
