2df03c76c374091d09a8fed6d603cb549361ef87
eahenle <eahenle@gmail.com>
2022-01-05 13:44:36 -0800

    compat Julia ^1.1

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,8 @@
 @assert VERSION.major == 1
-@assert VERSION.minor ≥ 0
+@assert VERSION.minor ≥ 1
 
 include("unit_tests.jl")
 
-if VERSION.minor == 7
+if VERSION.minor ≥ 7
     include("doc_tests.jl")
 end
