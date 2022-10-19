037165f6c06747ed63e7ad07f5dad786661e8be4
eahenle <eahenle@users.noreply.github.com>
2022-10-19 20:24:26 +0000

    JuliaFormatter Action Bot

--- src/cpk_colors.jl
+++ src/cpk_colors.jl
@@ -1,4 +1,8 @@
 # global cpk color dictionary
 # atom_symbol => (R, G, B)
 # built from PeriodicTable cpk_hex entires
-DEFAULT_CPK_COLORS = Dict(Symbol(elements[i].symbol) => ([parse(Int, elements[i].cpk_hex[x], base=16) for x in [[2,3],[4,5],[6,7]]]...,) for i in eachindex(elements) if length(elements[i].cpk_hex) == 7)
+DEFAULT_CPK_COLORS = Dict(
+    Symbol(elements[i].symbol) => (
+        [parse(Int, elements[i].cpk_hex[x]; base=16) for x in [[2, 3], [4, 5], [6, 7]]]...,
+    ) for i in eachindex(elements) if length(elements[i].cpk_hex) == 7
+)
